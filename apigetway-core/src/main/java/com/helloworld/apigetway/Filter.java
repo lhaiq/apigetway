a54a785687d922051bab4cb853dfcdb545424990
haiquan.li <P@ssw0rd1234>
2017-02-27 22:12:41 +0800

    init

---
+++ apigetway-core/src/main/java/com/helloworld/apigetway/Filter.java
@@ -0,0 +1,13 @@
+package com.helloworld.apigetway;
+
+/**
+ * Created by haiquan.li on 17/2/27.
+ */
+public abstract class Filter {
+
+    abstract public String filterType();
+
+    abstract public int filterOrder();
+
+
+}
---
+++ apigetway-core/src/main/java/com/helloworld/apigetway/context/RequestContext.java
@@ -0,0 +1,7 @@
+package com.helloworld.apigetway.context;
+
+/**
+ * Created by haiquan.li on 17/2/27.
+ */
+public class RequestContext {
+}
